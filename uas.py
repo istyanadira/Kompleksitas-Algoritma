d1d2aa3ce296b84ab07089a3fa812e1d243e302c
istyanadira <istyanadiraa@gmail.com>
2026-01-30 10:49:03 +0700

    Add files via upload

---
+++ uas.py
@@ -0,0 +1,27 @@
+INF = float('inf')
+
+def floyd_warshall(graph):
+    n = len(graph)
+    dist = [row[:] for row in graph]
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if dist[i][k] + dist[k][j] < dist[i][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+
+    return dist
+
+# Contoh graf (jarak antar kota)
+graph = [
+    [0, 3, INF, 7],
+    [8, 0, 2, INF],
+    [5, INF, 0, 1],
+    [2, INF, INF, 0]
+]
+
+result = floyd_warshall(graph)
+
+print("Matriks Jarak Terpendek:")
+for row in result:
+    print(row)
